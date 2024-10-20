221e1e1d92ef2cd16e5eda07730ce9b4f1fbd1bc
Josh Schreuder <681706+JoshSchreuder@users.noreply.github.com>
2024-10-20 14:11:27 +1100

    Split second skeleton

---
+++ src/st/no0/e_skeleton_2.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("st/no0/nonmatchings/e_skeleton_2", func_us_801D75E4);
+
+INCLUDE_ASM("st/no0/nonmatchings/e_skeleton_2", func_us_801D7670);
+
+INCLUDE_ASM("st/no0/nonmatchings/e_skeleton_2", func_us_801D7B40);
